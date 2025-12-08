d0f16f241c4b2ea7c9ca8595a80384a769054c9f
Swati Singh <swatisingh.it2023@nsec.ac.in>
2025-12-08 23:01:21 +0530

    u

--- proxy.js
+++ proxy.js
@@ -3,8 +3,3 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware({
   publicRoutes: ["/", "/sign-in", "/sign-up"],
 });
-
-export const runtime = "nodejs"; // force Node runtime
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
